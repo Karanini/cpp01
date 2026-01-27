1fe37b01f7eb4847acb7618aa96bdb81f636abfd
michel_32 <karas.benjamin@gmail.com>
2026-01-27 13:26:29 +0100

    ex01 ok

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 18:09:11 by michel_32         #+#    #+#             */
-/*   Updated: 2026/01/26 18:40:31 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/01/27 13:24:29 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 int main(void)
 {
-    Zombie  *Freddy = newZombie("Freddy");
-    Freddy->announce();
-    randomChump("Charles");
-    delete Freddy;
+    int nb_zombies = 32;
+
+    Zombie  *Freddy_hordy = zombieHorde(nb_zombies, "Freddy");
+    for (int i = 0; i < nb_zombies; i++)
+        Freddy_hordy[i].announce();
+    delete [] Freddy_hordy;
     return (0);
 }

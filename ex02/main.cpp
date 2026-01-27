71af45b6bf776c3eeddc1b91c1235b566e2278c6
michel_32 <karas.benjamin@gmail.com>
2026-01-27 17:31:12 +0100

    ex02 ok

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 13:27:29 by michel_32         #+#    #+#             */
-/*   Updated: 2026/01/27 13:33:06 by michel_32        ###   ########.fr       */
+/*   Updated: 2026/01/27 17:29:02 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,13 @@ int main(void)
     std::string *stringPTR = &hi_brain;
     std::string &stringREF = hi_brain;
 
-    std::cout << "memory address of the string variable: " << (void *)hi_brain << std::endl;
+    std::cout << "memory address of the string variable: " << &hi_brain << std::endl;
+    std::cout << "memory address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "memory address held by stringREF: " << &stringREF << std::endl;
+
+    std::cout << "value of the string variable: " << hi_brain << std::endl;
+    std::cout << "value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "value pointed to by stringREF: " << stringREF << std::endl;
+
+    return (0);
 }

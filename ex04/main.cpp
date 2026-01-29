f208d0f55436862cd8f46e8ce3212a3cf20702f6
bkaras-g <bkaras-g@student.42.fr>
2026-01-29 11:37:57 +0100

    add get_input to save input to str

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 14:29:08 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/01/29 10:56:19 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/01/29 11:12:46 by bkaras-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 #include <iostream>
 #include <fstream>
 
-// int	main(std::string filename, std::string s1, std::string s2)
-// {
-
-// }
+int	get_input(char *filename, std::string *str);
 
 int	main(int ac, char *av[])
 {
@@ -29,26 +26,40 @@ int	main(int ac, char *av[])
 
 	std::string		str;
 	std::string		filename = av[1];
-	filename.append(".replace");
-	std::ofstream	ofs(filename.c_str());
-	std::ifstream	ifs(av[1]);
-	if (!ifs.is_open())
-	{
-		std::cerr << "Error: Could not open file " << av[1] << std::endl;
-		return (1);
-	}
-
-	// std::string::iterator it = str.begin();
 	std::string s1 = av[2];
 	std::string s2 = av[3];
-	std::string tmp;
-	while(std::getline(ifs, tmp))
-	{
-		if (*str.begin())
-			str += '\n';
-		str += tmp;
-	}
+	filename.append(".replace");
+	std::ofstream	ofs(filename.c_str());
+
+	// std::string::iterator it = str.begin();
+
+	if (get_input(av[1], &str))
+		return (1);
 	std::cout << str;
 
 	return (0);
+}
+
+int	get_input(char *filename, std::string *str)
+{
+    std::ifstream	ifs(filename);
+
+    if (!ifs.is_open())
+    {
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        return (1);
+    }
+	if (ifs.peek() == std::ifstream::traits_type::eof())
+    {
+        std::cerr << "Error: File is empty" << std::endl;
+        return (1);
+    }
+    std::string tmp;
+    while(std::getline(ifs, tmp))
+    {
+        if (!str->empty())
+            *str += '\n';
+        *str += tmp;
+    }
+    return (0);
 }

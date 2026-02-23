9c21ceb9bd734c524b648e5972789693f0719602
michel_32 <karas.benjamin@gmail.com>
2026-02-23 13:55:26 +0100

    update comment to HumanA constructor

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HumanA.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bkaras-g <bkaras-g@student.42.fr>          +#+  +:+       +#+        */
+/*   By: michel_32 <michel_32@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 10:45:46 by bkaras-g          #+#    #+#             */
-/*   Updated: 2026/01/28 12:11:18 by bkaras-g         ###   ########.fr       */
+/*   Updated: 2026/02/23 13:54:54 by michel_32        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,10 @@ The variables are always initialized before the start of the constructor.
 Since references must be initialized upon creation and cannot be reassigned,
 (you can't use = in the constructor body),
 you must use the Member Initializer List.
+
+We pass a reference to weapon instead of passing by value, so that we keep the 
+adress of the weapon. If I make some changes to the weapon using setType(),the 
+changes are also reflected on the weapon held by HumanA. 
 */
 HumanA::HumanA(Weapon &weapon, const std::string &name) : _weapon(weapon), _name(name)
 {
